ab2788a3613ff4383cd77c5b68b373e3d1534833
amitthakkar <d0ntgetHyper>
2015-05-04 19:15:15 +0530

    Opening page in browser as task completly run.

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,13 @@
     var gulp = require('gulp'),
         browserify = require('gulp-browserify'),
         uglify = require('gulp-uglify'),
-        gulpif = require('gulp-if');
+        gulpif = require('gulp-if'),
+        open = require("gulp-open"),
+        runSequence = require('run-sequence');
     var isProduction = true;
+    gulp.task('setDevEnvironment', function () {
+        isProduction = false;
+    });
     gulp.task('browserifyAMD', function () {
         return gulp.src(['app/browserifyAMD.js'])
             .pipe(browserify())
@@ -25,10 +30,18 @@
             .pipe(gulpif(isProduction, uglify()))
             .pipe(gulp.dest('build/product'));
     });
-    gulp.task('browserify', ['browserifyAMD', 'browserifyHome', 'browserifyProduct']);
-    gulp.task('setDevEnvironment', function () {
-        isProduction = false;
+    gulp.task('browserify', function (callback) {
+        runSequence('browserifyAMD', 'browserifyHome', 'browserifyProduct', callback);
     });
-    gulp.task('dev', ['setDevEnvironment', 'browserify']);
+    gulp.task('open', function () {
+        var options = {
+            url: 'http://localhost:63342/AngularAMD-with-Browserify/'
+        };
+        gulp.src('./index.html')
+            .pipe(open("", options));
+    });
+    gulp.task('dev', function (callback) {
+        runSequence('setDevEnvironment', 'browserify', 'open', callback);
+    });
     gulp.task('default', ['browserify'])
 })(require);
