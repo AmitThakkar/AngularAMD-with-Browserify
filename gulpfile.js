82381882fde0240ba2cf751ce1787f61ffe4cfdd
amitthakkar <d0ntgetHyper>
2015-05-04 23:51:13 +0530

    Adding live reload task.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@
         gulpif = require('gulp-if'),
         open = require("gulp-open"),
         runSequence = require('run-sequence'),
+        livereload = require('gulp-livereload'),
         rimraf = require('rimraf');
 
     var isProduction = true;
@@ -21,19 +22,22 @@
         return gulp.src(['app/browserifyAMD.js'])
             .pipe(browserify())
             .pipe(gulpif(isProduction, uglify()))
-            .pipe(gulp.dest('build'));
+            .pipe(gulp.dest('build'))
+            .pipe(livereload());
     });
     gulp.task('browserifyHome', function () {
         return gulp.src(['app/home/home.controller.js'])
             .pipe(browserify())
             .pipe(gulpif(isProduction, uglify()))
-            .pipe(gulp.dest('build/home'));
+            .pipe(gulp.dest('build/home'))
+            .pipe(livereload());
     });
     gulp.task('browserifyProduct', function () {
         return gulp.src(['app/product/product.controller.js'])
             .pipe(browserify())
             .pipe(gulpif(isProduction, uglify()))
-            .pipe(gulp.dest('build/product'));
+            .pipe(gulp.dest('build/product'))
+            .pipe(livereload());
     });
     gulp.task('browserify', function (callback) {
         runSequence('browserifyAMD', 'browserifyHome', 'browserifyProduct', callback);
@@ -45,8 +49,14 @@
         gulp.src('./index.html')
             .pipe(open("", options));
     });
+    gulp.task('watch', function () {
+        livereload.listen();
+        gulp.watch('app/*.js', ['browserifyAMD']);
+        gulp.watch('app/home/*.js', ['browserifyHome']);
+        gulp.watch('app/product/*.js', ['browserifyProduct']);
+    });
     gulp.task('dev', function (callback) {
-        runSequence('clear', 'setDevEnvironment', 'browserify', 'open', callback);
+        runSequence('clear', 'setDevEnvironment', 'browserify', 'open', 'watch', callback);
     });
     gulp.task('default', function(callback) {
         runSequence('clear', 'browserify', callback);
