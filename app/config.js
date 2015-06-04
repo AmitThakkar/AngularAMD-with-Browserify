928ce8ead5492f33b0d146b0923739c14bdaf607
amitthakkar <d0ntgetHyper>
2015-06-04 10:22:37 +0530

    Config code clean up done.

--- app/config.js
+++ app/config.js
@@ -7,18 +7,25 @@
     var externalModules = [
         'ngRoute'
     ];
-    var homeApp = require('./home/home.main.js');
-    var productApp = require('./product/product.main.js');
-    var commonApp = require('./common/common.main.js');
-    var internalModules = [
-        homeApp.moduleName,
-        productApp.moduleName,
-        commonApp.moduleName
+    // If your module have route configuration then push module main file to moduleMainFiles.
+    var moduleMainFiles = [
+        require('./home/home.main.js'),
+        require('./product/product.main.js')
     ];
-    var internalModuleObjects = [
-        homeApp,
-        productApp
+    // If your module have not route configuration then don't neet push module to internalModuleObjects.
+    var withoutRouteMainFiles = [
+        require('./common/common.main.js')
     ];
+
+    var internalModules = [];
+    var internalModuleObjects = [];
+    moduleMainFiles.forEach(function (moduleMainFile) {
+        internalModules.push(moduleMainFile.moduleName);
+        internalModuleObjects.push(moduleMainFile);
+    });
+    withoutRouteMainFiles.forEach(function (withoutRouteMainFile) {
+        internalModules.push(withoutRouteMainFile.moduleName);
+    });
     exports.mainAppModule = MAIN_APP_MODULE;
     exports.dependModules = externalModules.concat(internalModules);
     exports.internalModuleObjects = internalModuleObjects;
